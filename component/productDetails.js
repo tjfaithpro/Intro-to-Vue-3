45706b348886ade28450c6ebf946f0cfd447b527
Tejiri Faith <tjfaithpro@gmail.com>
2022-07-23 13:58:44 +0100

    add component

--- component/productDetails.js
+++ component/productDetails.js
@@ -12,9 +12,4 @@ app.component("product-details", {
      <li v-for="detail in details">{{ detail }}</li>
     </ul>
     `,
-    data(){
-        return {
-            details: ['50% cotton', '30% wool', '20% polyester'],
-        }
-    }
 });
--- component/productDisplay.js
+++ component/productDisplay.js
@@ -39,7 +39,7 @@ app.component('product-display', {
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
-           
+            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
